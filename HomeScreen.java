b404bcfd3cecd92c79849d606391346cddf3f8d2
khristova21 <42977135+khristova21@users.noreply.github.com>
2019-05-23 13:10:13 -0400

    Add files via upload

---
+++ HomeScreen.java
@@ -0,0 +1,13 @@
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class HomeScreen extends JPanel
+{
+
+	HomeScreen()
+	{
+		JLabel homeScreen = new JLabel("Welcome to ......");
+		add(homeScreen);
+	}
+	
+}
